90db94ec76c5cf024cfdac5447cb14c8b0b1e2fd
pangil5634 <oksk6681@naver.com>
2025-11-10 15:15:12 +0900

    refactor : rename component to PublicLayout

--- src/routes/(public)/+page.svelte
+++ src/routes/(public)/+page.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-	import Layout from '$lib/components/layout/PublicLayout.svelte';
+	import PublicLayout from '$lib/components/layout/PublicLayout.svelte';
 	import HeroSection from '$lib/components/landing/HeroSection.svelte';
 	import ServiceIntroSection from '$lib/components/landing/ServiceIntroSection.svelte';
 	import CTASection from '$lib/components/landing/CTASection.svelte';
 </script>
 
-<Layout>
+<PublicLayout>
 	<HeroSection />
 	<ServiceIntroSection />
 	<CTASection />
-</Layout>
+</PublicLayout>
