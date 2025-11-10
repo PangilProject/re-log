79424ae277474e86d5efc73af0d1323aa83cb0fe
pangil5634 <oksk6681@naver.com>
2025-11-10 18:04:45 +0900

    feat: hide delete button while editing user profile
    회원정보 수정(editing) 중에는 회원 탈퇴 버튼이 표시되지 않도록
    class:hidden 디렉티브를 적용하여 UI 안정성을 개선함.

--- src/routes/(private)/mypage/+page.svelte
+++ src/routes/(private)/mypage/+page.svelte
@@ -136,6 +136,7 @@
 					<button
 						on:click={handleDelete}
 						class="rounded-lg bg-red-500 px-4 py-2 font-semibold text-white hover:bg-red-600"
+						class:hidden={editing}
 					>
 						회원 탈퇴
 					</button>
