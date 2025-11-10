6c85565db4f8af34a09afe2f5ee5e9bbb725e80e
pangil5634 <oksk6681@naver.com>
2025-11-10 10:36:55 +0900

    style : update section style
    마지막 CTA 부분에 대한 section의 배경색을 linear 그라디언트로 주어 통일성을 수정하였습니다.

--- src/routes/(public)/+page.svelte
+++ src/routes/(public)/+page.svelte
@@ -73,7 +73,7 @@
 		</section>
 
 		<!-- 3️⃣ 마무리 CTA -->
-		<section class="bg-blue-50 px-4 py-24 text-center">
+		<section class="bg-linear-to-b from-white to-blue-50 px-4 py-32 pt-60 text-center">
 			<h3 class="mb-6 text-3xl font-bold text-gray-800">지금 당신의 첫 회고를 기록해보세요.</h3>
 			<p class="mb-10 text-lg text-gray-600">
 				기억은 흐려지지만, 기록은 성장으로 남습니다. Re:Log로 당신의 성장을 함께 만들어가요.
