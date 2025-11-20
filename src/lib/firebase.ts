66dfdfa5093037bbcbbf1d1d52f4d689157b4a90
pangil5634 <oksk6681@naver.com>
2025-11-20 15:48:02 +0900

    chore : add analytics firebase
    firebase 분석 대쉬보드를 위한 기능을 연결했습니다.

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,6 +2,8 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import { getAnalytics, isSupported, type Analytics } from 'firebase/analytics';
+import { browser } from '$app/environment';
 
 const firebaseConfig = {
 	apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -9,10 +11,20 @@ const firebaseConfig = {
 	projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
 	storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
 	messagingSenderId: import.meta.env.VITE_FIREBASE_SENDER_ID,
-	appId: import.meta.env.VITE_FIREBASE_APP_ID
+	appId: import.meta.env.VITE_FIREBASE_APP_ID,
+	measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
+export let analytics: Analytics | null = null;
+
+if (browser) {
+	isSupported().then((supported) => {
+		if (supported) {
+			analytics = getAnalytics(app);
+		}
+	});
+}
 // db 관련 기능
 export const db = getFirestore(app);
 // 계정 관련 기능
